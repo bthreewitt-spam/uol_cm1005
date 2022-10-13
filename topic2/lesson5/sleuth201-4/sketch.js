fc6414ad22c28bcea42539fd9b3a027dd2828171
Brendan <brendanthreewitt@gmail.com>
2022-10-13 02:37:33 -0600

    completed project

--- topic2/lesson5/sleuth201-4/sketch.js
+++ topic2/lesson5/sleuth201-4/sketch.js
@@ -41,11 +41,32 @@ function draw()
     noFill();
 
     // write the code to draw around the Judge's body below
-
+    beginShape();
+    vertex(360,214);
+    vertex(375,215);
+    vertex(395,232);
+    vertex(475,229);
+    vertex(495,205);
+    vertex(562,183);
+    vertex(594,195);
+    vertex(628,237);
+    vertex(660,288);
+    vertex(690,231);
+    vertex(718,248);
+    vertex(668,328);
+    vertex(620,304);
+    vertex(603,491);
+    vertex(582,495);
+    vertex(570,573);
+    vertex(455,573);
+    vertex(510,264);
+    vertex(386,260);
+    vertex(351,225);
+    endShape(CLOSE);
 
     //A helpful mouse pointer
 	push();
-        fill(0);
+        fill(255,0,0);
         noStroke();
         text(mouseX + "," + mouseY, mouseX,mouseY);
     pop();
