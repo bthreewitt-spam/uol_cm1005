68e64b8389d196bf4c5daea2923830394bf03a04
Brendan <brendanthreewitt@gmail.com>
2022-10-14 08:51:54 -0600

    added coordinate helper

---
+++ CoordinateHelper/README.md
@@ -0,0 +1,5 @@
+# Coordinate Helper
+  
+## How to add to your project
+1) Add to same folder as the html file in your Project (ex. index.html)
+    
---
+++ CoordinateHelper/coordinatePointer.js
@@ -0,0 +1,18 @@
+function coordinatePointer(){
+
+    let textX = mouseX + 13;
+    let textY = mouseY - 10;
+    let rectX = mouseX + 10;
+    let rectY = mouseY - 22;
+    let textCoordinates = `${round(mouseX)},${round(mouseY)}`;
+    let rectWidth = 7 * textCoordinates.length + 1;
+
+    if(mouseY < 22) {rectY += 31; textY += 31;}
+    if(mouseX > width - 70) {rectX -= 80; textX -= 80;}
+
+    cursor('CoordinateHelper/crossHair.cur');
+    fill(0);
+    rect(rectX, rectY, rectWidth, 15, 5);
+    fill(255);
+    text(textCoordinates, textX, textY);
+}
