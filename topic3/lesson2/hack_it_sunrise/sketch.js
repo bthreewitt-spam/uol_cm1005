5debabc585ab9b5de1d04a82e6aa618d2155e1d6
Brendan <brendanthreewitt@gmail.com>
2022-10-17 04:05:11 -0600

    created sun,ground and sky

--- topic3/lesson2/hack_it_sunrise/sketch.js
+++ topic3/lesson2/hack_it_sunrise/sketch.js
@@ -1,95 +1,63 @@
-var groundHeight;
-var mountain1;
-var mountain2;
+let time = {current: 0, speedMS: 500, multiplier: 1};
 
-var tree;
-
-var moon;
-var sun;
-var darkness;
-
-function setup()
-{
-	createCanvas(800, 600);
-	//set the groundHeight proportional to the canvas size
-	groundHeight = (height / 3) * 2;
-
-	//initalise the mountain objects with properties to help draw them to the canvas
-	mountain1 = {
-		x: 400,
-		y: groundHeight,
-		height: 320,
-		width: 230
-	};
-	mountain2 = {
-		x: 550,
-		y: groundHeight,
-		height: 200,
-		width: 130
-	};
-
-	//initalise the tree object
-	tree = {
-		x: 150,
-		y: groundHeight + 20,
-		trunkWidth: 40,
-		trunkHeight: 150,
-		canopyWidth: 120,
-		canopyHeight: 100
-	};
-
-    //initalise the sun 
-	sun = {
-		x: 150,
-		y: 70,
-		diameter: 80,
-	};
-    
-    //TASK: intialise a moon object with an extra property for brightness
+//NOTE: Color of elements at 12
+//NOTE: All coordinate numbers are percentages of window width/height
+let sky = {r:0, g:204, b:254};
+let sun = {x:4, y:5, diameter:8, //diameter as % of width 
+	r:255, g:255, b:0};
+let ground = {x:0, y:65, 
+			r:0, g:200, b:0};
 
 
-	//set the initial darkness
-	darkness = 0;
-}
-
-
-
-function draw()
-{
-	//TASK: update the values for the moons brightness, the sun's position and the darkness.
-	//You can either map this to the mouse's location (i.e. the futher left the mouse is the more daylight) or you can just change the values gradually over time.
-
-
-	//draw the sky
-	background(150, 200, 255);
+function setup(){
+	createCanvas(windowWidth, windowHeight);
 	noStroke();
-
-	//draw the sun
-	fill(255, 255, 0);
-	ellipse(sun.x, sun.y, sun.diameter);
-    
-    //TASK: you'll need to draw the moon too. Make sure you use brightness to adjust the colour
-
-	//draw the ground and make it green
-	fill(70, 200, 0);
-	rect(0, groundHeight, width, height - groundHeight);
-
-	//draw the mountains
-	fill(120);
-	triangle(mountain1.x, mountain1.y,
-		mountain1.x + mountain1.width, mountain1.y,
-		mountain1.x + (mountain1.width / 2), mountain1.y - mountain1.height);
-
-	triangle(mountain2.x, mountain2.y,
-		mountain2.x + mountain2.width, mountain2.y,
-		mountain2.x + (mountain2.width / 2), mountain2.y - mountain2.height);
-    
-    //TASK: You can draw the tree yourself
-    
-
-	//TASK: make the scene dark by drawing a rectangle that covers the whole canvas.
-	//Use the alpha value of fill to determine how dark to make it
-
-
-
+	noLoop();
 }
+function draw(){
+	setColor();
+	
+	background(sky.r, sky.g, sky.b);
+	sun.draw();
+	ground.draw();
+
+	advanceTime();
+}
+
+
+
+ground.draw = () => {
+	push();
+	fill(ground.r, ground.g, ground.b);
+	rect(xPercent(ground.x), yPercent(ground.y), windowWidth, yPercent(ground.y));
+	pop();
+}
+
+sun.draw = () => {
+	push();
+	fill(sun.r, sun.g, sun.b);
+	
+	ellipse(xPercent(sun.x) , yPercent(sun.y), xPercent(sun.diameter));
+	
+	pop();
+}
+
+function setColor(){
+	
+}
+
+async function advanceTime() {
+	// TODO: create time menu
+	
+	console.log(time.current);
+	await sleep((time.speedMS * time.multiplier));
+	
+	time.current += 1;
+	if (time.current === 24) {time.current = 0;}
+	
+	redraw();
+}
+
+function sleep(ms) {return new Promise(resolve => setTimeout(resolve, ms));}
+function xPercent(percent){return percent / 100 * windowWidth;}
+function yPercent(percent){return percent / 100 * windowHeight;}
