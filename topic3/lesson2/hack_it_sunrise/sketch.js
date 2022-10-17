7c6c1d36f7233cf606ab2404c79499d44b3402c8
Brendan <brendanthreewitt@gmail.com>
2022-10-17 05:26:42 -0600

    added moon and sun/moon cycle

--- topic3/lesson2/hack_it_sunrise/sketch.js
+++ topic3/lesson2/hack_it_sunrise/sketch.js
@@ -1,10 +1,12 @@
-let time = {current: 0, speedMS: 500, multiplier: 1};
+let time = {current: 0, waitMS: 500, multiplier: 1, framesPer:120};
 
-//NOTE: Color of elements at 12
+//NOTE: Color of elements at 6
 //NOTE: All coordinate numbers are percentages of window width/height
 let sky = {r:0, g:204, b:254};
-let sun = {x:4, y:5, diameter:8, //diameter as % of width 
-	r:255, g:255, b:0};
+let sun = {path:92, diameter:8, //as % of width 
+			r:255, g:255, b:0};
+let moon = {path:sun.path, diameter:sun.diameter, 
+			r:255, g:255, b:255}; //TODO: 
 let ground = {x:0, y:65, 
 			r:0, g:200, b:0};
 
@@ -19,6 +21,7 @@ function draw(){
 	
 	background(sky.r, sky.g, sky.b);
 	sun.draw();
+	moon.draw();
 	ground.draw();
 
 	advanceTime();
@@ -35,10 +38,24 @@ ground.draw = () => {
 
 sun.draw = () => {
 	push();
-	fill(sun.r, sun.g, sun.b);
-	
-	ellipse(xPercent(sun.x) , yPercent(sun.y), xPercent(sun.diameter));
-	
+	stroke(sun.r, sun.g, sun.b);
+	strokeWeight(xPercent(sun.diameter));
+	noFill();
+	arc(xPercent(50), yPercent(ground.y), 
+		xPercent(sun.path), xPercent(sun.path), 
+		PI + time.current / 12 * PI,
+		PI + time.current / 12 * PI + .0001);
+	pop();
+}
+moon.draw = () => {
+	push();
+	stroke(moon.r, moon.g, moon.b);
+	strokeWeight(xPercent(moon.diameter));
+	noFill();
+	arc(xPercent(50), yPercent(ground.y),
+		xPercent(moon.path), xPercent(moon.path),
+		PI + (time.current - 12) / 12 * PI,
+		PI + (time.current - 12) / 12 * PI + .0001);
 	pop();
 }
 
@@ -50,10 +67,10 @@ async function advanceTime() {
 	// TODO: create time menu
 	
 	console.log(time.current);
-	await sleep((time.speedMS * time.multiplier));
+	await sleep((time.waitMS / time.multiplier)/time.framesPer);
 	
-	time.current += 1;
-	if (time.current === 24) {time.current = 0;}
+	time.current += 1/time.framesPer;
+	if (time.current > 24) {time.current = 0;}
 	
 	redraw();
 }
