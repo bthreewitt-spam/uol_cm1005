0ebbbfae313c1af304276ccadd29552982444f33
Brendan <brendanthreewitt@gmail.com>
2022-10-19 03:02:57 -0600

    completed project

--- topic3/lesson5/gp2a - Game Character/sketch.js
+++ topic3/lesson5/gp2a - Game Character/sketch.js
@@ -8,7 +8,7 @@ function setup(){
 function draw(){
     background(255);
 
-
+    //////////////////////////1////////////////////////////
     stroke(100);
     noFill();
     rect(20,60,50,80);
@@ -18,33 +18,32 @@ function draw(){
 
     gameChar_x = 45;
     gameChar_y = 137;
-
-    //Front facing - code goes here
-    // body
-    fill(30)
-    arc(gameChar_x, gameChar_y-25, 20, 40, PI-1, 1, CHORD);
-    //legs
+    //////////////////code goes here///////////////////////
+    
+    fill(30);
+    arc(gameChar_x, gameChar_y-25, 20, 40, PI-1, 1, CHORD); // body
+    
     stroke(30);
     strokeWeight(5)
-    line(gameChar_x-5, gameChar_y-2, gameChar_x-5, gameChar_y-12);//left
-    line(gameChar_x+5, gameChar_y-2, gameChar_x+5, gameChar_y-12);//right
-    //arms
-    line(gameChar_x-11, gameChar_y-20, gameChar_x-8, gameChar_y-35); //left
-    line(gameChar_x+11, gameChar_y-20, gameChar_x+8, gameChar_y-35); //right
+    line(gameChar_x-5, gameChar_y-2, gameChar_x-5, gameChar_y-12);//left leg
+    line(gameChar_x+5, gameChar_y-2, gameChar_x+5, gameChar_y-12);//right leg
+    line(gameChar_x-11, gameChar_y-20, gameChar_x-8, gameChar_y-35); //left arm
+    line(gameChar_x+11, gameChar_y-20, gameChar_x+8, gameChar_y-35); //right arm
     noStroke();
-    // head
+    
     push();
     fill(253, 209, 167);
-    ellipse(gameChar_x, gameChar_y-50, 30);
+    ellipse(gameChar_x, gameChar_y-50, 30); // head
     pop();
-    arc(gameChar_x, gameChar_y-50, 29, 30, PI+.25, -.25, CHORD);
-    arc(gameChar_x, gameChar_y-50, 30, 31, .25, PI, CHORD);
+    
+    arc(gameChar_x, gameChar_y-50, 29, 30, PI+.25, -.25, CHORD); //top of mask
+    arc(gameChar_x, gameChar_y-50, 30, 31, .25, PI, CHORD); //bottom of mask
+    
     fill(0);
     ellipse(gameChar_x - 5 , gameChar_y-51, 4); // left eye
     ellipse(gameChar_x + 5 , gameChar_y-51, 4); // right eye
-
-
-
+    
+    //////////////////////////2////////////////////////////
     stroke(100);
     noFill();
     rect(200,60,50,80);
@@ -54,12 +53,32 @@ function draw(){
 
     gameChar_x = 225;
     gameChar_y = 137;
+    //////////////////code goes here///////////////////////
 
-    //Jumping forward - code goes here
+    fill(30);
+    arc(gameChar_x, gameChar_y-25, 20, 40, PI-1, 1, CHORD); // body
 
+    stroke(30);
+    strokeWeight(5)
+    line(gameChar_x-8, gameChar_y-2, gameChar_x-5, gameChar_y-12);//left leg
+    line(gameChar_x+8, gameChar_y-2, gameChar_x+5, gameChar_y-12);//right leg
+    line(gameChar_x-17, gameChar_y-45, gameChar_x-8, gameChar_y-35); //left arm
+    line(gameChar_x+17, gameChar_y-45, gameChar_x+8, gameChar_y-35); //right arm
+    noStroke();
+
+    push();
+    fill(253, 209, 167);
+    ellipse(gameChar_x, gameChar_y-50, 30); // head
+    pop();
+
+    arc(gameChar_x, gameChar_y-50, 29, 30, PI+.25, -.25, CHORD); //top of mask
+    arc(gameChar_x, gameChar_y-50, 30, 31, .25, PI, CHORD); //bottom of mask
+
+    fill(0);
+    ellipse(gameChar_x - 5 , gameChar_y-51, 4); // left eye
+    ellipse(gameChar_x + 5 , gameChar_y-51, 4); // right eye
     
-    
-    
+    //////////////////////////3////////////////////////////
     stroke(100);
     noFill();
     rect(20,260,50,80);
@@ -69,12 +88,30 @@ function draw(){
 
     gameChar_x = 45;
     gameChar_y = 337;
+    //////////////////code goes here///////////////////////
 
-    //Walking left - code goes here
+    fill(30);
+    arc(gameChar_x, gameChar_y-25, 20, 40, PI-1, 1, CHORD); // body
 
+    stroke(30);
+    strokeWeight(5)
+    line(gameChar_x-4, gameChar_y-2, gameChar_x-4, gameChar_y-12);//left leg
+    line(gameChar_x+3, gameChar_y-2, gameChar_x+3, gameChar_y-12);//right leg
+    line(gameChar_x+11, gameChar_y-20, gameChar_x+7, gameChar_y-35); //right arm
+    noStroke();
 
+    push();
+    fill(253, 209, 167);
+    ellipse(gameChar_x, gameChar_y-50, 30); // head
+    pop();
+
+    arc(gameChar_x, gameChar_y-50, 29, 30, PI+.25, -.25, CHORD); //top of mask
+    arc(gameChar_x, gameChar_y-50, 29, 31, -.5, PI-.15, CHORD); //bottom of mask
+
+    fill(0);
+    ellipse(gameChar_x - 11 , gameChar_y-51, 4); // left eye
     
-    
+    //////////////////////////4////////////////////////////
     stroke(100);
     noFill();
     rect(200,260,50,80);
@@ -84,12 +121,29 @@ function draw(){
 
     gameChar_x = 225;
     gameChar_y = 337;
+    //////////////////code goes here///////////////////////
+    fill(30);
+    arc(gameChar_x, gameChar_y-25, 20, 40, PI-1, 1, CHORD); // body
 
-    //Walking right - code goes here
+    stroke(30);
+    strokeWeight(5)
+    line(gameChar_x-3, gameChar_y-2, gameChar_x-3, gameChar_y-12);//left leg
+    line(gameChar_x+4, gameChar_y-2, gameChar_x+4, gameChar_y-12);//right leg
+    line(gameChar_x-11, gameChar_y-20, gameChar_x-8, gameChar_y-35); //left arm
+    noStroke();
 
+    push();
+    fill(253, 209, 167);
+    ellipse(gameChar_x, gameChar_y-50, 30); // head
+    pop();
 
-    
-    
+    arc(gameChar_x, gameChar_y-50, 29, 30, PI+.25, -.25, CHORD); //top of mask
+    arc(gameChar_x, gameChar_y-50, 30, 31, +.15, PI+.5, CHORD); //bottom of mask
+
+    fill(0);
+    ellipse(gameChar_x + 11 , gameChar_y-51, 4); // right eye
+
+    //////////////////////////5////////////////////////////
     stroke(100);
     noFill();
     rect(20,460,50,80);
@@ -99,12 +153,30 @@ function draw(){
 
     gameChar_x = 45;
     gameChar_y = 537;
+    //////////////////code goes here///////////////////////
 
-    //Jumping right - code goes here
+    fill(30);
+    arc(gameChar_x, gameChar_y-25, 20, 40, PI-1, 1, CHORD); // body
 
+    stroke(30);
+    strokeWeight(5)
+    line(gameChar_x-8, gameChar_y-2, gameChar_x-3, gameChar_y-12);//right leg
+    line(gameChar_x+11, gameChar_y-4, gameChar_x+4, gameChar_y-12);//left leg
+    line(gameChar_x-14, gameChar_y-20, gameChar_x-8, gameChar_y-35); //right arm
+    noStroke();
 
+    push();
+    fill(253, 209, 167);
+    ellipse(gameChar_x, gameChar_y-50, 30); // head
+    pop();
 
+    arc(gameChar_x, gameChar_y-50, 29, 30, PI+.25, -.25, CHORD); //top of mask
+    arc(gameChar_x, gameChar_y-50, 30, 31, +.15, PI+.5, CHORD); //bottom of mask
 
+    fill(0);
+    ellipse(gameChar_x + 11 , gameChar_y-51, 4); // right eye
+
+    //////////////////////////6////////////////////////////
     stroke(100);
     noFill();
     rect(200,460,50,80);
@@ -114,10 +186,26 @@ function draw(){
 
     gameChar_x = 225;
     gameChar_y = 537;
+    //////////////////code goes here///////////////////////    
 
-    //Jumping left - code goes here
-    
-    
-    
-    
+    fill(30);
+    arc(gameChar_x, gameChar_y-25, 20, 40, PI-1, 1, CHORD); // body
+
+    stroke(30);
+    strokeWeight(5)
+    line(gameChar_x+8, gameChar_y-2, gameChar_x+3, gameChar_y-12);//left leg
+    line(gameChar_x-11, gameChar_y-4, gameChar_x-4, gameChar_y-12);//right leg
+    line(gameChar_x+14, gameChar_y-20, gameChar_x+8, gameChar_y-35); //left arm
+    noStroke();
+
+    push();
+    fill(253, 209, 167);
+    ellipse(gameChar_x, gameChar_y-50, 30); // head
+    pop();
+
+    arc(gameChar_x, gameChar_y-50, 29, 30, PI+.25, -.25, CHORD); //top of mask
+    arc(gameChar_x, gameChar_y-50, 29, 31, -.5, PI-.15, CHORD); //bottom of mask
+
+    fill(0);
+    ellipse(gameChar_x - 11 , gameChar_y-51, 4); // left eye
 }
