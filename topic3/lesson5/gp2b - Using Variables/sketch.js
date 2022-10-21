320a983fdd54e03460304b23ac5eaa8693fc47f0
Brendan <brendanthreewitt@gmail.com>
2022-10-21 09:19:40 -0600

    cleaned up formatting

--- topic3/lesson5/gp2b - Using Variables/sketch.js
+++ topic3/lesson5/gp2b - Using Variables/sketch.js
@@ -1,12 +1,4 @@
-/*
-
-The Game Project
-
-2b - using variables
-
-*/
-
-let floorPos_y;
+let canyon, collectable, mountain, cloud;
 
 let gameChar_x;
 let gameChar_y;
@@ -14,19 +6,13 @@ let gameChar_y;
 let treePos_x;
 let treePos_y;
 
-let canyon;
-let collectable;
-
-let mountain;
-let cloud;
-
+let floorPos_y;
 
 function setup()
 {
 	createCanvas(1024, 576);
-	floorPos_y = 432; //NB. we are now using a variable for the floor position
-
-	//NB. We are now using the built-in variables height and width
+	floorPos_y = 432;
+	
 	cloud = {x_pos: 250, 
 			 y_pos: 150, 
 			 width: 250, 
@@ -50,9 +36,7 @@ function setup()
 	gameChar_x = width/2;
 	gameChar_y = floorPos_y;
 }
-
-function draw()
-{
+function draw() {
 	background(100, 155, 255); //fill the sky blue
 
 	noStroke();
@@ -66,7 +50,6 @@ function draw()
 	drawCollectable();
 	drawGameChar();
 }
-
 function mousePressed() {
 	gameChar_x = mouseX;
 	gameChar_y = mouseY;
@@ -117,46 +100,75 @@ function drawMountain(){
 		 mountain.y_pos - mountain.height * .69);					//LEFT___BOTTOM_Y
 }
 function drawTree(){
+	//////////////////////////TRUNK////////////////////////////
 	fill(146, 86, 63);
-	rect(treePos_x-13.5,treePos_y-100, 27,100); //trunk main
+	rect(treePos_x-13.5,treePos_y-100, 27,100);
+	/////////////////////////DETAIL////////////////////////////
+						/////TRUNK/////
 	fill(103, 58, 49);
-	triangle(treePos_x-13.5,treePos_y,treePos_x+13.5,treePos_y,treePos_x+13.5,treePos_y-50);//trunk detail
+	triangle(treePos_x-13.5,treePos_y,				//LEFT
+			 treePos_x+13.5,treePos_y,				//RIGHT
+			 treePos_x+13.5,treePos_y-50);		//TOP
+						/////LEAFS/////
 	fill(58, 87, 34);
-	rect(treePos_x-27.5,treePos_y-94,58,20, 30); //leaf side detail
-	ellipse(treePos_x+.5,treePos_y-79,26,17); //bottom center leaves
+	rect(treePos_x-27.5,treePos_y-94,58,20, 30); 	//SIDE
+	ellipse(treePos_x+.5,treePos_y-79,26,17);			//BOTTOM
+	/////////////////////////LEAFS/////////////////////////////
 	fill(89, 150, 30);
-	rect(treePos_x-43.5,treePos_y-140,90,60, 17); //main leaves
-	rect(treePos_x-28.5,treePos_y-95,60,20, 17); //bottom side leaves
-	ellipse(treePos_x+.5,treePos_y-80,28,17); //bottom center leaves
-	rect(treePos_x-48.5,treePos_y-135,100,40, 17); //middle side leaves
-	rect(treePos_x-36.5,treePos_y-144,75,40, 15); //top side leaves
-	ellipse(treePos_x+.5,treePos_y-132,50,40); //top center leaves
+	rect(treePos_x-43.5,treePos_y-140,90,60, 17);	//MAIN
+	rect(treePos_x-28.5,treePos_y-95,60,20, 17); 	//BOTTOM_SIDE
+	ellipse(treePos_x+.5,treePos_y-80,28,17);			//BOTTOM_CENTER
+	rect(treePos_x-48.5,treePos_y-135,100,40, 17);	//MIDDLE_SIDE
+	rect(treePos_x-36.5,treePos_y-144,75,40, 15); 	//MIDDLE_CENTER
+	ellipse(treePos_x+.5,treePos_y-132,50,40);			//__TOP__CENTER
 }
 function drawCanyon(){
+	/////////////////////////////LIGHT///////////////////////////////
 	fill(33, 29, 35);
-	quad(canyon.x_pos+1,432,canyon.width+canyon.x_pos,432,canyon.width+canyon.x_pos-33,576,canyon.x_pos-67,576);//abyss light
+	quad(canyon.x_pos+1,432,					//LEFT__TOP
+		 canyon.width+canyon.x_pos, 432,	//RIGHT_TOP
+		 canyon.width+canyon.x_pos-33,576,	//RIGHT_BOTTOM
+		 canyon.x_pos-67,576);				//LEFT__BOTTOM
+	/////////////////////////////DARK////////////////////////////////
 	fill(0)
-	quad(canyon.x_pos,441,canyon.width+canyon.x_pos-3,441,canyon.width+canyon.x_pos-35,576,canyon.x_pos-65,576);//abyss dark
+	quad(canyon.x_pos,441, 						//LEFT__TOP
+	 canyon.width+canyon.x_pos-3,441,		//RIGHT_TOP
+	 canyon.width+canyon.x_pos-35,576, 		//RIGHT_BOTTOM
+	 canyon.x_pos-65,576);					//LEFT__BOTTOM
+	/////////////////////////////DIRT////////////////////////////////
 	fill(113, 54, 51);
-	triangle(canyon.x_pos+1,439,canyon.x_pos-67,576,canyon.x_pos-69.5,576);//dirt
+	triangle(canyon.x_pos+1,439,				//TOP____CENTER
+			 canyon.x_pos-67,576,			//BOTTOM_RIGHT
+			 canyon.x_pos-69.5,576);		//BOTTOM_LEFT
 }
 function drawCollectable(){
 	push();
+	
 	rectMode(CENTER);
 	let s = collectable.size/49
 	scale(s);
+	
+	//////////////////////////////////OUTER//////////////////////////////////////
+								//////BLACK//////
 	fill(1);
-	ellipse(collectable.x_pos/s,collectable.y_pos/s,48,49); //outer black outline
+	ellipse(collectable.x_pos/s,collectable.y_pos/s,48,49);
+								//////WHITE//////
 	fill(255);
-	ellipse((collectable.x_pos-5)/s,collectable.y_pos/s,31,37); //outer white outline
+	ellipse((collectable.x_pos-5)/s,collectable.y_pos/s,31,37);
+								//////GOLD///////
 	fill(252, 192, 5);
-	ellipse((collectable.x_pos-1)/s,(collectable.y_pos+1)/s,37,40); //main gold
+	ellipse((collectable.x_pos-1)/s,(collectable.y_pos+1)/s,37,40);
+	//////////////////////////////////INNER//////////////////////////////////////
+								//////BLACK//////
 	fill(1);
-	rect((collectable.x_pos-1.5)/s,(collectable.y_pos+3)/s,9,20,2); // inner black outline
+	rect((collectable.x_pos-1.5)/s,(collectable.y_pos+3)/s,9,20,2);
+								//////WHITE//////
 	fill(255);
-	rect((collectable.x_pos-3)/s,(collectable.y_pos+1.5)/s,10,21,2); // inner white outline
+	rect((collectable.x_pos-3)/s,(collectable.y_pos+1.5)/s,10,21,2);
+								//////GOLD///////
 	fill(257, 198, 15);
-	rect((collectable.x_pos-2.5)/s,(collectable.y_pos+2)/s,9,20,2); // inner gold
+	rect((collectable.x_pos-2.5)/s,(collectable.y_pos+2)/s,9,20,2);
+	
 	pop()
 }
 function drawGameChar(){
