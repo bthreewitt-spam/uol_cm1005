ab4ab210aab7d06eb4d9f92d655e3b63685d6683
Brendan <brendanthreewitt@gmail.com>
2022-10-21 15:48:54 -0600

    added

---
+++ topic4/lesson5/sleuth403-4/sketch.js
@@ -0,0 +1,67 @@
+/*
+
+Officer: 4747203
+CaseNum: 403-3-97579589-4747203
+
+Case 403 - Captured - stage 4
+
+A coordinated action is under way to arrest Shiffman. Police are currently in pursuit on Meyers Way.
+In order to catch him we must be able to alert all forces of his whereabouts according to the following rules:
+
+- if Shiffman is within 133 meters from Torvald's House then alert local police by drawing a Indigo circle around it with a radius of 133 pixels.
+- if Shiffman is in Wrong Side of the tracks then the neighbourhood watch must be notified by drawing a Chocolate rectangle around it.
+- if Shiffman is in neither position, a global alert must be issued by drawing a PaleVioletRed rectangle covering the area between Ada Avenue, Gates Avenue, Meyers Way and Mullenweg Street.
+
+Shiffman's position is signified by the mouse.
+
+Note: all road coordinates are measured from their center.
+
+Use X11 colours. You can find a reference table at https://www.w3.org/TR/css3-iccprof#numerical.
+
+There are many possible ways of investigating this case, but you should use ONLY the following commands, operators and variables:
+
+  if(){}
+  >
+  <
+  &&
+  else
+  fill()  - Use r,g,b values between 0 and 255.
+  dist()
+  ellipse()
+  rect()
+  mouseX
+  mouseY
+
+*/
+
+var img;
+
+function preload()
+{
+	img = loadImage('map.jpg');
+}
+
+function setup()
+{
+	createCanvas(img.width,img.height);
+}
+
+function draw()
+{
+    // draw the image
+    image(img,0,0);
+
+    //Write your code below here ...
+
+    // finally, draw Shiffman's position
+    strokeWeight(2);
+    stroke(255);
+    fill(255,0,0);
+    ellipse(mouseX, mouseY, 10, 10);
+
+
+    // a helpful mouse coordinate pointer
+    fill(255);
+    noStroke();
+    text(`${mouseX},${mouseY}`,mouseX, mouseY);
+}
